ecb2a3793efdfd2bfe3397ba58577cc990245195
Julian Khatibi <ajkhatibi@gmail.com>
2016-10-14 14:44:22 -0700

    first jquery implemetation done

---
+++ assets/javascript/javascript.js
@@ -0,0 +1,11 @@
+$("#character1").click(function(){
+	$("#character2").css("clear", "both");
+});
+
+$("#character4").click(function(){
+	$("#character4").css("clear", "both");
+});
+
+function restartPage(){
+	window.location.reload();
+}
