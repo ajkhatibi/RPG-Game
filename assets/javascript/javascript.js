42ea32ff40775bb213d4c2e347ee8efea04bbee1
Julian Khatibi <ajkhatibi@gmail.com>
2016-10-16 18:09:41 -0700

    character done

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,11 +1,44 @@
-$("#character1").click(function(){
-	$("#character2").css("clear", "both");
-});
+function restart() {
+        window.location.reload();
+    }
 
-$("#character4").click(function(){
-	$("#character4").css("clear", "both");
-});
+$(document).ready(function() {
+    var index;
+    var hp;
 
-function restartPage(){
-	window.location.reload();
-}
+
+    $("#chooseChar img").click(function() {
+        var index = $("#chooseChar img").index(this);
+        var hp = $(this).attr("value")
+        console.log(hp);
+        console.log(index);
+        if (index === 0) {
+            $("#chooseChar").hide();
+            $("#yours").append('<img id="img1" src="assets/images/Ben_kenobi.png">');
+            $("#enemies").append('<img id="img2" src="assets/images/Darth-Vader.jpg">');
+            $("#enemies").append('<img id="img3" src="assets/images/luke.jpg">');
+            $("#enemies").append('<img id="img4" src="assets/images/maul.jpg">');
+
+        } else if (index === 1){
+            $("#chooseChar").hide();
+            $("#yours").append('<img id="img2" value="200" src="assets/images/Darth-Vader.jpg">');
+            $("#enemies").append('<img id="img1" value="500" src="assets/images/Ben_kenobi.png">');
+            $("#enemies").append('<img id="img3" value="300" src="assets/images/luke.jpg">');
+            $("#enemies").append('<img id="img4" value="350" src="assets/images/maul.jpg">');
+        } else if (index === 2){
+            $("#chooseChar").hide();
+            $("#yours").append('<img id="img3" value="300" src="assets/images/luke.jpg">');
+            $("#enemies").append('<img id="img1" value="500" src="assets/images/Ben_kenobi.png">');
+            $("#enemies").append('<img id="img2" value="200" src="assets/images/Darth-Vader.jpg">');
+            $("#enemies").append('<img id="img4" value="350" src="assets/images/maul.jpg">');
+        } else if (index === 3){
+            $("#chooseChar").hide();
+            $("#yours").append('<img id="img4" value="350" src="assets/images/maul.jpg">');
+            $("#enemies").append('<img id="img1" value="500" src="assets/images/Ben_kenobi.png">');
+            $("#enemies").append('<img id="img2" value="200" src="assets/images/Darth-Vader.jpg">');
+            $("#enemies").append('<img id="img3" value="300" src="assets/images/luke.jpg">');
+        }
+    });
+
+
+});
