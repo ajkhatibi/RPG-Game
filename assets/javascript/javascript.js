c51f1d57b02ba687b56f974f341e9dbe6cea0c2a
Julian Khatibi <ajkhatibi@gmail.com>
2016-10-16 20:32:51 -0700

    almost done

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -28,20 +28,29 @@ $(document).ready(function() {
             $("#yours").append('<img id="img2" value="200" src="assets/images/Darth-Vader.jpg">');
             $("#enemies").append('<img id="img1" value="500" src="assets/images/Ben_kenobi.png">');
             $("#enemies").append('<img id="img3" value="300" src="assets/images/luke.jpg">');
-            $("#enemies").append('<img id="img4" value="350" src="assets/images/maul.jpg">');
+            $("#enemies").append('<img id="img4" value="300" src="assets/images/maul.jpg">');
         } else if (index === 2) {
             $("#chooseChar").hide();
             $("#yours").append('<img id="img3" value="300" src="assets/images/luke.jpg">');
             $("#enemies").append('<img id="img1" value="500" src="assets/images/Ben_kenobi.png">');
             $("#enemies").append('<img id="img2" value="200" src="assets/images/Darth-Vader.jpg">');
-            $("#enemies").append('<img id="img4" value="350" src="assets/images/maul.jpg">');
+            $("#enemies").append('<img id="img4" value="300" src="assets/images/maul.jpg">');
         } else if (index === 3) {
             $("#chooseChar").hide();
-            $("#yours").append('<img id="img4" value="350" src="assets/images/maul.jpg">');
+            $("#yours").append('<img id="img4" value="300" src="assets/images/maul.jpg">');
             $("#enemies").append('<img id="img1" value="500" src="assets/images/Ben_kenobi.png">');
             $("#enemies").append('<img id="img2" value="200" src="assets/images/Darth-Vader.jpg">');
             $("#enemies").append('<img id="img3" value="300" src="assets/images/luke.jpg">');
         }
+
+        // $("#enemies img").click(function(){
+        //     var index2 = $("#enemies img").index(this);
+        //     if (index2 === 0) {
+        //     $("#img1").hide();
+        //     $("#battle").append('<img id="img1" src="assets/images/Ben_kenobi.png">');
+
+        //     }
+        // })
 
         $("#attack").click(function(){
 
@@ -53,6 +62,10 @@ $(document).ready(function() {
             console.log(clicks)
             } else if (hp < 1){
                 alert("you lose")
+            }
+
+            if (clicks > 4){
+                alert("you win");
             }
 
         })
